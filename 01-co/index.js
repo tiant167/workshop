35544cbcd2732ff3cdaf0aee037e04a2bbc95267
Haotian Chai <haotianchai@wandoujia.com>
2015-06-30 19:45:53 +0800

    finished 01-co

--- 01-co/index.js
+++ 01-co/index.js
@@ -12,7 +12,11 @@ var fs = require('fs');
  */
 
 exports.stat = function (filename) {
-
+  return function(callback){
+    fs.stat(filename, function(err, content){
+      callback(err, content);
+    });
+  }
 };
 
 /**
@@ -32,5 +36,10 @@ exports.stat = function (filename) {
  */
 
 exports.exists = function (filename) {
-
+  return function(callback){
+    fs.stat(filename, function(err, content){
+      if(err) callback(null, false);
+      else callback(null, true);
+    });
+  }
 };

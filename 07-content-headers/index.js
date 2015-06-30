5349fea98a716be67968787bf56ae71a0d3b1958
Haotian Chai <haotianchai@wandoujia.com>
2015-06-30 23:03:33 +0800

    finished 07

--- 07-content-headers/index.js
+++ 07-content-headers/index.js
@@ -4,5 +4,9 @@ var koa = require('koa');
 var app = module.exports = koa();
 
 app.use(function* () {
-  
+  if(this.request.is('json') === false){
+    this.response.body = 'ok';
+  } else {
+    this.response.body = {'message': 'hi!'};
+  }
 })
